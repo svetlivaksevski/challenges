ebf405a3d8d90693dc1293bbcf7b7b2b8c50348c
Svetoslav Vaksevski <svetoslav.vaksevski@Svetoslavs-MacBook-Air.local>
2024-02-09 16:25:56 +0100

    Solved the solution

---
+++ Calculate-circle-area-and-circumference/index.js
@@ -0,0 +1,6 @@
+const radius = 5;
+const circleArea = Math.PI * Math.pow(radius, 2)
+const circumference = 2 * Math.PI * radius;
+
+console.log("For a circle with a radius of units:", radius,"\n\tThe area is approximately",circleArea,"square units.",
+"\n\t- The circumference is approximately", circumference ,"units.")
