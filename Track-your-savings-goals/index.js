9569c1bb9a5d1db2f76900624bec7b231c306aaf
Svetoslav Vaksevski <svetoslav.vaksevski@Svetoslavs-MacBook-Air.local>
2024-02-09 16:07:08 +0100

    Solved the challenge

---
+++ Track-your-savings-goals/index.js
@@ -0,0 +1,7 @@
+const startingBalance = 700;
+const montlyContribution = 200;
+const months = 12;
+
+totalSavings = startingBalance + (montlyContribution * months)
+
+console.log("After 12 months of saving, you will have", totalSavings ,"in your account");
