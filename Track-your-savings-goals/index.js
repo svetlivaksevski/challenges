c2bf38ae4487a67b4ac4fbc818b7e72217644f07
Svetoslav Vaksevski <svetoslav.vaksevski@Svetoslavs-MacBook-Air.local>
2024-02-09 16:32:53 +0100

    Add change in months

--- Track-your-savings-goals/index.js
+++ Track-your-savings-goals/index.js
@@ -4,4 +4,4 @@ const months = 12;
 
 totalSavings = startingBalance + (montlyContribution * months)
 
-console.log("After 12 months of saving, you will have", totalSavings ,"in your account");
+console.log("After",months,"months of saving, you will have", totalSavings ,"in your account");
