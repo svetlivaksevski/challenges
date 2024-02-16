f6ecf4cd1ea5815c252e37174f623fff093e0350
Svetoslav Vaksevski <svetoslav.vaksevski@Svetoslavs-MacBook-Air.local>
2024-02-16 10:54:22 +0100

    Solved the challenge

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,19 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+    console.log(e);
+
+	const formData = new FormData(e.target);
+	const data = Object.fromEntries(formData);
+   
+	console.log(data);
+
+	e.target.reset();
+	e.target.elements.firstName.focus();
+
+	console.log(`The age-badness-sum of ${form.firstName.value} is ${parseInt(form.age.value) + parseInt(form.badness.value)}`);
+
+})
