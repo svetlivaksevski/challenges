9187b27eb8e1d19dc9d46821c969c2b7026758a7
svetlivaksevski <svetlivaksevski@gmail.com>
2024-02-20 17:12:52 +0100

    Solved the challenge

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -2,12 +2,21 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars = 0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let i = 1; i <= 5; i++) {
+ 
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", filledStars < i ? "./assets/star-empty.svg" : "./assets/star-filled.svg");
+    imgElement.addEventListener("click", () => {
+        renderStars(i);
+    });
+    starContainer.appendChild(imgElement);
+  
+  }
   //--^-- your code here --^--
 }
 
