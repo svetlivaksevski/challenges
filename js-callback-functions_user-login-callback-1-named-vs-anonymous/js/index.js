308fb1ea0f578f70475579d7ba76e52e29059c91
svetlivaksevski <svetlivaksevski@gmail.com>
2024-02-21 10:20:29 +0100

    Solved the challenge

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -6,3 +6,15 @@ function handleUserLogin(onSuccess) {
 }
 
 // Call handleUserLogin below!
+
+function showWelcomeMessage(x) {
+  console.log('Welcome! You are logged in now.')
+}
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {});
+
+handleUserLogin(() => {
+  console.log('Welcome! You are logged in now.')
+});
