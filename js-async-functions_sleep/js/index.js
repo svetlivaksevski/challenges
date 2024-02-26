904a3fe91a35d0fca36832b3c47b0011f73cee99
svetlivaksevski <svetlivaksevski@gmail.com>
2024-02-26 15:21:57 +0100

    Remove extra code that wasn't necessary

--- js-async-functions_sleep/js/index.js
+++ js-async-functions_sleep/js/index.js
@@ -7,27 +7,27 @@ const body = document.body;
 // --v-- code here --v--
 
 button3.addEventListener("click", async () => {
-  document.body.classList.add('dark');
+
   icon.textContent = "💤";
   await sleep(3);
   icon.textContent = "⏰";
-  document.body.classList.remove('dark');
+ 
 });
 
 button5.addEventListener("click", async () => {
-  document.body.classList.add('dark');
+  
   icon.textContent = "💤";
   await sleep(5);
   icon.textContent = "⏰";
-  document.body.classList.remove('dark');
+
 });
 
 async function handleSleepFor10Button() {
-  document.body.classList.add('dark');
+
   icon.textContent = "💤";
   await sleep(10);
   icon.textContent = "⏰";
-  document.body.classList.remove('dark');
+
 }
 
 button10.addEventListener("click", handleSleepFor10Button);
@@ -36,8 +36,10 @@ button10.addEventListener("click", handleSleepFor10Button);
 
 // returns a Promise that resolves after x seconds
 function sleep(seconds) {
+  document.body.classList.add('dark');
   const sleepPromise = new Promise((resolve) => {
-    setTimeout(() => resolve(), seconds * 1000);
+    setTimeout(() => { document.body.classList.remove('dark');
+  resolve()}, seconds * 1000);
   });
   return sleepPromise;
 }
