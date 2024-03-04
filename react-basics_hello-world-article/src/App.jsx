431a1ad87f8ac8b68bf8c09a4c4bbe4bfa802f7a
svetlivaksevski <svetlivaksevski@gmail.com>
2024-03-04 15:14:16 +0100

    Solved the challenge

--- react-basics_hello-world-article/src/App.jsx
+++ react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello world!</h1>
+      <p>It's a sunny day.</p>
+    </article>
+  );
 }
