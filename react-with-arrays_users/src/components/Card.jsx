7ddff966911e8599881ccce1124a82770a40b40b
svetlivaksevski <svetlivaksevski@gmail.com>
2024-03-07 12:13:51 +0100

    Solved the challenge

--- react-with-arrays_users/src/components/Card.jsx
+++ react-with-arrays_users/src/components/Card.jsx
@@ -6,7 +6,9 @@ function Card({ user }) {
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {user.roles.map((role, index) => {
+          return <Tag tag={role} key={index} />;
+        })}
       </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
