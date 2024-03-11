308c67e0af866cf34e873b8cfd825e48bc1ed16b
svetlivaksevski <svetlivaksevski@gmail.com>
2024-03-11 11:08:36 +0100

    Solved the challenge

--- react-state-2_table-reservation/src/components/Counter.jsx
+++ react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import "./Counter.css";
 
-
-export default function Counter() {
-  const [people, setPeople] = useState(0);
+export default function Counter({ onHandleIncrement, onHandleDecrement }) {
+  // const [people, setPeople] = useState(0);
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
@@ -12,7 +10,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={onHandleIncrement}
         >
           +
         </button>
@@ -20,7 +18,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={onHandleDecrement}
         >
           -
         </button>
