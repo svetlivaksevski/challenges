f8cdbe73312e947fee4983c3210925bae35b37a5
svetlivaksevski <svetlivaksevski@gmail.com>
2024-03-23 13:11:36 +0100

    Solved the challenge

---
+++ react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,43 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  console.log("result", data);
+  if (error) return <div>Not found</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
